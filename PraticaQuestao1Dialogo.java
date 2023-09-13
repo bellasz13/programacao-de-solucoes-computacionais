b7bcaa787b4fd5c1291a4d3aafdc8928cd1e394c
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-13 20:45:02 -0300

    Prática 1 - Questão 1 (caixa de diálogo)

---
+++ PraticaQuestao1Dialogo.java
@@ -0,0 +1,22 @@
+package praticaquest1dialog;
+
+import javax.swing.JOptionPane;
+
+public class PraticaQuestao1Dialogo {
+
+	public static void main(String[] args) {
+       
+        String nota1Str = JOptionPane.showInputDialog("Digite a primeira nota:");
+        double nota1 = Double.parseDouble(nota1Str);
+
+        String nota2Str = JOptionPane.showInputDialog("Digite a segunda nota:");
+        double nota2 = Double.parseDouble(nota2Str);
+
+        String nota3Str = JOptionPane.showInputDialog("Digite a terceira nota:");
+        double nota3 = Double.parseDouble(nota3Str);
+
+        double media = (nota1 + nota2 + nota3) / 3.0;
+
+        JOptionPane.showMessageDialog(null, "A média das notas é: " + media);
+}
+}
