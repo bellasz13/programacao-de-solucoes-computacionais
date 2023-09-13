3450c10081311267a49c60b96baa75512a3897ab
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-13 18:56:39 -0300

    PraticaQuestao2.java

--- PraticaQuestao2.java
+++ PraticaQuestao2.java
@@ -5,22 +5,17 @@ public class PraticaQuestao2 {
 	    public static void main(String[] args) {
 	        Scanner input = new Scanner(System.in);
 
-	        // Solicita o ano de nascimento
 	        System.out.print("Digite o ano de nascimento: ");
 	        int anoNascimento = input.nextInt();
 
-	        // Solicita o ano atual
 	        System.out.print("Digite o ano atual: ");
 	        int anoAtual = input.nextInt();
-	        
-	        // Calcula a idade no ano atual
+	     
 	        int idadeAtual = anoAtual - anoNascimento;
 
-	        // Calcula a idade em 2050
 	        int ano2050 = 2050;
 	        int idade2050 = ano2050 - anoNascimento;
 
-	        // Imprime as informações
 	        System.out.println("A idade da pessoa no ano atual é: " + idadeAtual + " anos.");
 	        System.out.println("A idade que a pessoa terá em 2050 será: " + idade2050 + " anos.");
 
