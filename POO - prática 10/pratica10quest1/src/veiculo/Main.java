2007ac7a675198a77bd3017d396b83f3a775181e
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-10-11 18:30:32 -0300

    Prática 10

---
+++ POO - prática 10/pratica10quest1/src/veiculo/Main.java
@@ -0,0 +1,16 @@
+package veiculo;
+
+public class Main {
+	public static void main(String[] args) {
+      
+        Cadastro cadastro = new Cadastro(10);
+
+        Veiculo veiculo1 = new Veiculo("Ford", "Fiesta", "12345ABCDE", "XYZ123", "Azul");
+        Veiculo veiculo2 = new Veiculo("Toyota", "Corolla", "67890FGHIJ", "ABC456", "Prata");
+
+        cadastro.cadastrarVeiculo(veiculo1);
+        cadastro.cadastrarVeiculo(veiculo2);
+
+        cadastro.imprimirCadastro();
+	}
+}
